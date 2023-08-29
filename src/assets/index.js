ea0d420fada4205953d6c82daaf939a3416623fc
Rajuaravind Sankar <rajuaravindsankar@Rajuaravinds-MacBook-Pro.local>
2023-08-29 20:16:10 +0530

    init

---
+++ src/assets/index.js
@@ -0,0 +1,107 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import tryhackme from "./tryhackme.svg";
+import hackthebox from "./hackthebox.svg";
+import splunk from "./splunk.svg";
+import cribl from "./cribl.svg";
+import positka from "./positka-logo.webp";
+import letsdefend from "./letsdefend-logo.webp";
+import vel from "./vel.webp";
+import clg from "./clg.svg";
+import scl from "./school-bag.svg";
+import zybeak from "./zybeak.svg";
+
+import wireshark from "./Wireshark.svg";
+import mircosoft from "./Microsoft.svg";
+import linux from "./linux.webp";
+import openvpn from "./openvpn.svg";
+import kali from "./kali.svg";
+import gcp from "./gcp.webp";
+import burpsuite from "./burpsuite.svg";
+import aws from "./aws.webp";
+import splunk1 from "./splunk.webp";
+import postman from "./postman.svg";
+import cod from "./cod.webp";
+
+export {
+  cod,
+  splunk1,
+  postman,
+  aws,
+  wireshark,
+  mircosoft,
+  linux,
+  openvpn,
+  kali,
+  gcp,
+  burpsuite,
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  tryhackme,
+  hackthebox,
+  splunk,
+  cribl,
+  positka,
+  letsdefend,
+  vel,
+  clg,
+  scl,
+  zybeak,
+
+};
+
---
+++ src/constants/index.js
@@ -0,0 +1,405 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  tryhackme,
+  hackthebox,
+  splunk,
+  cribl,
+  positka,
+  letsdefend,
+  clg,
+  scl,
+  zybeak,
+  wireshark,
+  mircosoft,
+  linux,
+  openvpn,
+  kali,
+  gcp,
+  burpsuite,
+  aws,
+  splunk1,
+  postman,
+  cod
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Experience",
+  },
+];
+
+const services = [
+  {
+    title: "Splunk Accredited Splunk Cloud and Enterprise Developer",
+    icon: web,
+  },
+  {
+    title: "Cribl Certified Observability Engineer (CCOE) Stream User",
+    icon: mobile,
+  },
+  {
+    title: "EC council Certified Ethical Hacker version 11",
+    icon: backend,
+  },
+  {
+    title: "Top 4% of Tryhackme Users",
+    icon: creator,
+  },
+  {
+    title: "#590 Global Rank in HacktheBox",
+    icon: creator,
+  },
+  {
+    title: "Business English Certificate (BEC)",
+    icon: backend,
+  },
+  {
+    title: "Splunk Accredited Security Sales Rep 1",
+    icon: mobile,
+  },
+  {
+    title: "Splunk Support Engineer (SSE)",
+    icon: web,
+  },
+];
+
+const technologies = [
+  {
+    name: "AWS",
+    icon: aws,
+  },
+  {
+    name: "Splunk",
+    icon: linux,
+  },
+  {
+    name: "Postman",
+    icon: gcp,
+  },
+  {
+    name: "Kubernetes",
+    icon: docker,
+  },
+  {
+    name: "Mircosoft",
+    icon: mircosoft,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: wireshark,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: splunk1,
+  },
+  {
+    name: "Node JS",
+    icon: postman,
+  },
+  {
+    name: "MongoDB",
+    icon: cod,
+  },
+  {
+    name: "Three JS",
+    icon: openvpn,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: burpsuite,
+  },
+  {
+    name: "docker",
+    icon: kali,
+  },
+];
+
+const experiences = [
+  {
+    title: "M.sc in CyberSecurity",
+  
+    company_name: "National College of Ireland",
+    icon: clg,
+    date: "2023 - 2024",
+    iconBg: "#0E0F0F",
+   
+    points: [ 
+    
+     
+    ],
+  },
+  {
+    title: "Junior Analyst - Splunk Team",
+    company_name: "Positka",
+    icon: positka,
+    iconBg: "#180202",
+    date: "May 2022 - Aug 2023",
+    points: [
+      "Experienced in Splunk Support Tickets as Splunk Support Engineer (SSE).",
+      "Integrating AWS Elastic Kubernetes Service into Splunk Enterprise using Lamada Function.",
+      "Knowledge about Splunk architecture and various components (indexer, forwarder, search head, deployment server), Heavy and Universal forwarder, License mode",
+      "Configuring Splunk distributed environment with Universal Forwarder, Heavy Forwarder, Indexer , Search Head.",
+      "Configuring Splunk SAML Authentication with OKTA.",
+      "Splunk App development ",
+      "Developed Splunk App with Splunk Dashboard Studio.",
+      "Configuring search Head Cluster with search peers and deployer",
+      "Integrating Solar wind Orionnew logs into Splunk using Syslog.",
+      "Upgrading Splunk versions, Cluster Rolling Upgrade.",
+      "efficient Splunk search Queries.",
+    ],
+  },
+  {
+    title: "Cribl Achievement",
+  
+    icon: cribl,
+    iconBg: "#1C1F23",
+   
+    points: [ 
+      "Cribl Certified Observability Engineer (CCOE) Stream User",
+     
+    ],
+  },
+  {
+    title: "Splunk Accreditations",
+  
+    icon: splunk,
+    iconBg: "#070F07",
+   
+    points: [ 
+      "Splunk Accredited Splunk Cloud and Enterprise Developer",
+      "Splunk Accredited Sales Rep 1",
+      "Splunk Accredited Sales Rep 2",
+      "Splunk Accredited Security Sales Rep 1",
+     
+    ],
+  },
+  {
+    title: "Lets Defend Badges",
+  
+    icon: letsdefend,
+    iconBg: "#000707",
+   
+    points: [ 
+      "First Blood - Investigated the first incident",
+      "SOC Member - Completed the SOC Fundamentals course",
+      "Phishing Expert - Phishing Campaign Analysis course",
+      "Incident Handler - Investigated 5 different incidents",
+     
+    ],
+  },
+  {
+    title: "Networking Traineer - Part Time",
+    company_name: "Zybeak Technologies",
+    icon: zybeak,
+    iconBg: "#E6DEDD",
+    date: "Apr 2022 - Jul 2022 ",
+    points: [
+      "Classes on networking , OSI Model , Ip Addressing (ipv4 & ipv6)  and Mac Addressing mechanisms ",
+      "Explaining TCP  Protocol, UDP Protocol ,Tor network",
+      "basic Classes on  Linux Operating system.",
+       "Explaining OSWAP 10, Cryptography , Hashing",
+    ],
+  },
+  {
+    title: "TryHackme Badges",
+  
+    icon: tryhackme,
+    iconBg: "#111010",
+   
+    points: [ 
+      "BLUE - Hacking into Windows via Eternal blue.",
+      "HASH CRACKER - Cracking all those Hashes.",
+      "OSWAP TOP 10 - Understanding very OSWAP vulnerability.",
+      "ICE - Exploiting Windows via Media Server.",
+      "PENTESTING PRINCIPLES - Completing Pentesting Introduction",
+      "Module. CAT LINUX.TXT - Being Competent in Linux.",
+      "MR.ROBOT - Completing the Mr.Robot room.",
+      "7 DAY STREAK - Achieving a 7 day Hacking Streak",
+     
+    ],
+  },
+  {
+    title: "HackTheBox Pwned Boxes",
+  
+    icon: hackthebox,
+    iconBg: "#001C00",
+   
+    points: [ 
+      "EXPLORE",
+      "PANDORA",
+      "CAT",
+      "BOUNTY HUNTER",
+      "MEOW",
+      "PAPER",
+      "BACKDOOR",
+      "SECRET",
+      "PREVISE",
+     
+    ],
+  },
+  {
+    title: "B.Tech - Information Technology",
+    company_name: "St.Joseph's Institute Of Technology",
+    icon: clg,
+    iconBg: "#0E0F0F",
+    date: "Sept 2018 - April 2022",
+    points: [
+      "Qualified First Class with 7.5 CGPA",
+      "RIPShell - Reverse Shell Generator in Commad Line Interface by using Python Programming Language",
+      "Business English Certificate - BEC",
+      
+    ],
+  },
+  {
+    title: "HSC Graduation",
+    company_name: "Velammal Matric Higher Secondary School",
+    icon: scl,
+    iconBg: "#E6DEDD",
+    date: "2017 - 2018",
+    points: [
+      "Qualified with 78 Percentage",
+     
+    ],
+  },
+
+  {
+    title: "SSLC Graduation",
+    company_name: "Velammal Matric Higher Secondary School",
+    icon: scl,
+    iconBg: "#E6DEDD",
+    date: "2015 - 2016",
+    points: [
+      "Qualified with 96 Percentage",
+      
+    ],
+  },
+];
+
+
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
