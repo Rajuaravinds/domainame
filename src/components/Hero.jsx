06df3e77fb4cb9d2cb279190f8cc461629f4b2ce
Rajuaravind Sankar <125336591+Rajuaravinds@users.noreply.github.com>
2023-12-25 01:27:11 +0000

    color gradient update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,22 +10,27 @@ const Hero = () => {
         className={`absolute inset-0 top-[100px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#AD1FC4]' />
+          <div className='w-5 h-5 rounded-full bg-gradient-to-r from-white to-gray-500' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#FBFBFB]'>Rajuaravind Sankar</span>
+            Hi, I'm  <span
+    className='text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-pink-600'
+  >Rajuaravind Sankar</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          <span className='text-[#AD1FC4] font-verdana'  style={{ fontSize: '1.8rem' }}>Cybersecurity Analyst </span> 
+          <span
+    className='text-transparent bg-clip-text bg-gradient-to-r from-white to-gray-500 font-verdana'
+    style={{ fontSize: '1.8rem' }}
+  >Cybersecurity Analyst </span> 
           </p>
          
         </div>
       </div>
 
-      <ComputersCanvas />
+      <ComputersCanvas  />
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
@@ -35,7 +40,7 @@ const Hero = () => {
                 y: [0, 24, 0],
               }}
               transition={{
-                duration: 1.5,
+                duration: 1,
                 repeat: Infinity,
                 repeatType: "loop",
               }}
